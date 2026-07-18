ec48865facfef4363e6b964a2ca19097ef0d29ef
the-ayush-ch0udhary <choudharyayush811@gmail.com>
2026-07-18 21:38:11 +0530

    Test deployment

--- users/views.py
+++ users/views.py
@@ -1,27 +1,9 @@
-from rest_framework import generics
-from .models import UserProfile
-from .serializers import UserProfileSerializer
-from .permissions import IsOwner
-from .filters import UserProfileFilter
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
 
+class UserListCreateView(APIView):
+    permission_classes = [AllowAny]
 
-class UserListCreateView(generics.ListCreateAPIView):
-    serializer_class = UserProfileSerializer
-
-    filterset_class = UserProfileFilter
-    search_fields = ["user__username", "bio"]
-    ordering_fields = ["id"]
-
-    def get_queryset(self):
-        return UserProfile.objects.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
-
-
-class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = UserProfileSerializer
-    permission_classes = [IsOwner]
-
-    def get_queryset(self):
-        return UserProfile.objects.filter(user=self.request.user)
+    def get(self, request):
+        return Response({"message": "API is working"})
